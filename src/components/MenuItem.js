4c783ac13a0a4f02cf028c64f462ed610138e593
Daniel Melchor <dmelchor@localhost.localdomain>
2021-04-20 03:32:55 -0400

    Menu closes when clicking section

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,48 +1,65 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import { Link } from 'react-scroll'
+import { Link } from "react-scroll";
 
 export default class MenuItem extends Component {
-    state = {
-        active: false
-    }
+  state = {
+    active: false,
+  };
 
-    componentDidMount() {
-        this.onScroll();
-        window.addEventListener('scroll', this.onScroll.bind(this))
-    }
+  componentDidMount() {
+    this.onScroll();
+    window.addEventListener("scroll", this.onScroll.bind(this));
+  }
 
-    // Checks window scroll to see if a menu element should be highlighted
-    onScroll() {
-        let thisHeight = document.getElementById(this.props.linkTo).clientHeight;
-        let thisOffsetTop = document.getElementById(this.props.linkTo).offsetTop
-        let offset = window.innerHeight/3
-        if((window.scrollY + offset > thisOffsetTop) && (window.scrollY + offset < thisOffsetTop + thisHeight)) {
-            this.setState({active:true})
-        } else {
-            this.setState({active:false})
+  // Checks window scroll to see if a menu element should be highlighted
+  onScroll() {
+    let thisHeight = document.getElementById(this.props.linkTo).clientHeight;
+    let thisOffsetTop = document.getElementById(this.props.linkTo).offsetTop;
+    let offset = window.innerHeight / 3;
+    if (
+      window.scrollY + offset > thisOffsetTop &&
+      window.scrollY + offset < thisOffsetTop + thisHeight
+    ) {
+      this.setState({ active: true });
+    } else {
+      this.setState({ active: false });
+    }
+  }
+
+  render() {
+    return (
+      <Link
+        onClick={() => this.props.toggleMenuFunc()}
+        to={this.props.linkTo}
+        smooth={true}
+        duration={400}
+        delay={0}
+        spy={true}
+        offset={window.innerWidth < 768 ? -84 : 0}
+        ignoreCancelEvents={true}
+        style={
+          window.innerHeight > window.innerWidth
+            ? { paddingTop: "2vh", paddingBottom: "2vh" }
+            : { paddingTop: "2vh", paddingBottom: "2vh" }
         }
-    }
-
-    render() {
-        return (
-            <Link
-                to={this.props.linkTo}
-                smooth={true}
-                duration={400}
-                delay={0}
-                spy={true}
-                offset={window.innerWidth < 768 ? -84 : 0}
-                ignoreCancelEvents={true}
-                style={
-                    window.innerHeight > window.innerWidth?{paddingTop:"2vh",paddingBottom:"2vh"}:{paddingTop:"2vh",paddingBottom:"2vh"}
-                }
-                className={(this.state.active ? "cursor-pointer px-3 pl-4 2xl:pl-6 block w-100 bg-gray-100 shadow-md relative menuItemActive -mr-2 transition-all text-gray-900 border-l-8 border-secondary dark:bg-gray-700 dark:text-gray-100 dark:border-primaryDark" 
-                                             : "cursor-pointer px-3 pl-4 2xl:pl-6 block w-100 bg-gray-200 transition-all hover:bg-gray-100 text-gray-700 hover:text-black dark:bg-gray-800 dark:text-gray-400 dark:hover:text-gray-100 dark:border-primaryDark" )}
-            >
-                <i className={this.props.icon + " inline-block align-middle text-2xl 2xl:text-3xl pr-5 w-12 text-center text-primary dark:text-gray-400"}></i>
-                <span className="inline-block align-middle text-md xl:text-lg font-mono" ><span className="text-secondary dark:text-secondaryDark">this</span>{ this.props.text }</span>
-            </Link>
-        )
-    }
+        className={
+          this.state.active
+            ? "cursor-pointer px-3 pl-4 2xl:pl-6 block w-100 bg-gray-100 shadow-md relative menuItemActive -mr-2 transition-all text-gray-900 border-l-8 border-secondary dark:bg-gray-700 dark:text-gray-100 dark:border-primaryDark"
+            : "cursor-pointer px-3 pl-4 2xl:pl-6 block w-100 bg-gray-200 transition-all hover:bg-gray-100 text-gray-700 hover:text-black dark:bg-gray-800 dark:text-gray-400 dark:hover:text-gray-100 dark:border-primaryDark"
+        }
+      >
+        <i
+          className={
+            this.props.icon +
+            " inline-block align-middle text-2xl 2xl:text-3xl pr-5 w-12 text-center text-primary dark:text-gray-400"
+          }
+        ></i>
+        <span className="inline-block align-middle text-md xl:text-lg font-mono">
+          <span className="text-secondary dark:text-secondaryDark">this</span>
+          {this.props.text}
+        </span>
+      </Link>
+    );
+  }
 }
--- src/containers/Menu.js
+++ src/containers/Menu.js
@@ -1,127 +1,194 @@
-import React, { Component } from 'react'
-import MenuItem from '../components/MenuItem'
+import React, { Component } from "react";
+import MenuItem from "../components/MenuItem";
 
-import grid from '../img/grid2.png'
-import darkGrid from '../img/darkGrid.png'
+import grid from "../img/grid2.png";
+import darkGrid from "../img/darkGrid.png";
 
 export default class Menu extends Component {
-    // State manages if the menu is open + how the dark-mode icon looks
-    state = {
-        menuActive: false,
-        moon: "fas fa-moon"
+  // State manages if the menu is open + how the dark-mode icon looks
+  state = {
+    menuActive: false,
+    moon: "fas fa-moon",
+  };
+
+  constructor(props) {
+    super(props);
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+
+  // When the user clicks the hamburguer menu
+  toggleMenu() {
+    if (window.innerWidth < 640)
+      this.setState({ menuActive: !this.state.menuActive });
+  }
+
+  // Checking for the theme item in localStorage or they have their device dark-mode
+  // activated to set the appropiate theme
+  componentDidMount() {
+    let current = localStorage.getItem("theme");
+    if (current === null || current === "dark") {
+      this.setState({ moon: "far fa-moon" });
+      document.body.style.backgroundImage = "url(" + darkGrid + ")";
+      document.documentElement.classList.add("dark");
+    } else {
+      this.setState({ moon: "fas fa-moon" });
+      document.body.style.backgroundImage = "url(" + grid + ")";
+      document.documentElement.classList.remove("dark");
     }
 
-    // When the user clicks the hamburguer menu
-    toggleMenu() {
-        this.setState({ menuActive: !this.state.menuActive })
+    window.addEventListener("resize", this.handleResize.bind(this));
+    if (window.innerWidth >= 640) {
+      this.setState({ menuActive: true });
     }
+  }
 
-    // Checking for the theme item in localStorage or they have their device dark-mode
-    // activated to set the appropiate theme
-    componentDidMount() {
-        let current = localStorage.getItem('theme')
-        if (current === null || current === 'dark') {
-            this.setState({ moon: 'far fa-moon' });
-            document.body.style.backgroundImage = "url(" + darkGrid + ")";
-            document.documentElement.classList.add('dark');
-        } else {
-            this.setState({ moon: 'fas fa-moon' });
-            document.body.style.backgroundImage = "url(" + grid + ")";
-            document.documentElement.classList.remove('dark');
-        }
-
-        window.addEventListener('resize', this.handleResize.bind(this))
-        if (window.innerWidth >= 640) {
-            this.setState({ menuActive: true });
-        }
+  // Only for visuals checking responsiveness
+  handleResize() {
+    if (window.innerWidth >= 640) {
+      this.setState({ menuActive: true });
+    } else {
+      this.setState({ menuActive: false });
     }
+  }
 
-    // Only for visuals checking responsiveness
-    handleResize() {
-        if (window.innerWidth >= 640) {
-            this.setState({ menuActive: true })
-        } else {
-            this.setState({ menuActive: false })
-        }
+  // When the user clicks the dark-mode icon
+  changeTheme() {
+    let current = localStorage.getItem("theme");
+    if (current === null || current === "dark") {
+      localStorage.setItem("theme", "light");
+      document.documentElement.classList.remove("dark");
+      this.setState({ moon: "fas fa-moon" });
+      document.body.style.backgroundImage = "url(" + grid + ")";
+    } else if (current === "light") {
+      localStorage.setItem("theme", "dark");
+      document.documentElement.classList.add("dark");
+      this.setState({ moon: "far fa-moon" });
+      document.body.style.backgroundImage = "url(" + darkGrid + ")";
     }
+  }
 
-    // When the user clicks the dark-mode icon
-    changeTheme() {
-        let current = localStorage.getItem('theme')
-        if (current === null || current === 'dark') {
-            localStorage.setItem('theme', 'light');
-            document.documentElement.classList.remove('dark');
-            this.setState({ moon: 'fas fa-moon' });
-            document.body.style.backgroundImage = "url(" + grid + ")";
-        } else if (current === 'light') {
-            localStorage.setItem('theme', 'dark');
-            document.documentElement.classList.add('dark');
-            this.setState({ moon: 'far fa-moon' });
-            document.body.style.backgroundImage = "url(" + darkGrid + ")";
-        }
-    }
-
-    render() {
-        return (
-            <>
-                {/* This is the small screen menu */}
-                <div className="font-mono fixed md:hidden top-0 w-full z-30 text-center shadow-2xl bg-gray-100 dark:bg-gray-900 py-5 sm:p-5 border-b-4 border-primary dark:border-primaryDark">
-                    <div className="relative h-10">
-                        <i onClick={() => this.toggleMenu()} className="left-8 absolute fas fa-bars text-3xl sm:text-4xl sm:px-3 text-primary dark:text-primaryDark inline-block align-middle"></i>
-                        <a className="text-xl sm:text-2xl text-gray-800 dark:text-gray-300 inline-block align-middle absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2" href="/">
-                            <span className="text-blue-700 dark:text-blue-300">class</span> User(<span className="text-secondary dark:text-secondaryDark">DM</span>):
-                        </a>
-                        <div
-                            className="right-8 absolute transition-none md:hidden rounded shadow-2xl p-2 sm:p-3 cursor-pointer text-center bg-primary text-md dark:text-secondaryDark
+  render() {
+    return (
+      <>
+        {/* This is the small screen menu */}
+        <div className="font-mono fixed md:hidden top-0 w-full z-30 text-center shadow-2xl bg-gray-100 dark:bg-gray-900 py-5 sm:p-5 border-b-4 border-primary dark:border-primaryDark">
+          <div className="relative h-10">
+            <i
+              onClick={() => this.toggleMenu()}
+              className="left-8 absolute fas fa-bars text-3xl sm:text-4xl sm:px-3 text-primary dark:text-primaryDark inline-block align-middle"
+            ></i>
+            <a
+              className="text-xl sm:text-2xl text-gray-800 dark:text-gray-300 inline-block align-middle absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2"
+              href="/"
+            >
+              <span className="text-blue-700 dark:text-blue-300">class</span>{" "}
+              User(
+              <span className="text-secondary dark:text-secondaryDark">DM</span>
+              ):
+            </a>
+            <div
+              className="right-8 absolute transition-none md:hidden rounded shadow-2xl p-2 sm:p-3 cursor-pointer text-center bg-primary text-md dark:text-secondaryDark
                                        dark:bg-gray-600 border-b-2 border-gray-900 dark:border-gray-500 hover:bg-gray-900 dark:hover:bg-gray-700 inline-block align-middle text-secondary"
-                            onClick={this.changeTheme.bind(this)}
-                        >
-                            <i className={this.state.moon + " block"}></i>
-                        </div>
-                    </div>
-                </div>
+              onClick={this.changeTheme.bind(this)}
+            >
+              <i className={this.state.moon + " block"}></i>
+            </div>
+          </div>
+        </div>
 
-                {/* Dark mode icon (normal screens only) */}
-                <div
-                    className="transition-none hidden md:block z-50 py-4 px-7 cursor-pointer text-center bg-primary text-md fixed right-0
+        {/* Dark mode icon (normal screens only) */}
+        <div
+          className="transition-none hidden md:block z-50 py-4 px-7 cursor-pointer text-center bg-primary text-md fixed right-0
                                dark:text-secondaryDark dark:bg-gray-600 border-b-2 border-l-2  border-secondary dark:border-secondaryDark
                                hover:bg-gray-900 dark:hover:bg-gray-700 text-secondary"
-                    onClick={this.changeTheme.bind(this)}
-                >
-                    <i className={this.state.moon + " block"}></i>
-                </div>
+          onClick={this.changeTheme.bind(this)}
+        >
+          <i className={this.state.moon + " block"}></i>
+        </div>
 
-                {/* Normal screen menu */}
-                <div
-                    className="font-mono w-5/6 md:w-1/3 lg:w-1/4 2xl:w-1/5 shadow-2xl fixed h-screen z-40 bg-gray-200 dark:bg-gray-800 transition-transform md:transition-none md:transition-colors"
-                    style={this.state.menuActive ? {} : { transform: "translateX(-105%)" }}
-                >
-                    <div className="hidden md:block border-b-4 border-secondary dark:border-secondaryDark text-center">
-                        <div className="text-primary dark:text-blue-300 font-bold text-2xl inline-block py-5 font-sans" >danielmelchor.com</div>
-                    </div>
-                    <div className="md:top-1/2 md:absolute transform md:-translate-y-2/4 w-full">
-                        <div className="w-full pt-5 flex flex-wrap items-center justify-center hidden md:block">
-                            <a className="pl-5 text-xl w-full text-gray-800 dark:text-gray-300" href="/">
-                                <span className="text-green-900 dark:text-blue-300">class</span> User(<span className="text-secondary dark:text-secondaryDark">D<span className="hidden xl:inline">aniel</span>M</span>):
-                            </a>
-                        </div>
-                        <div className="pt-10 md:pt-2 xl:pt-5">
-                            <div className="md:hidden z-20 w-full mb-2 block px-3 text-center">
-                                <p className="text-3xl inline-block align-middle text-primary dark:text-gray-300">Menu</p>
-                                <i onClick={() => this.toggleMenu()} className="fas fa-times text-3xl float-right inline-block align-middle text-primary dark:text-primaryDark"></i>
-                            </div>
-                            <MenuItem text=".welcome()" icon="fab fa-creative-commons-zero" linkTo="welcome" />
-                            <MenuItem text=".about()" icon="fas fa-user" linkTo="about" />
-                            <MenuItem text=".experience()" icon="fas fa-user-tie" linkTo="experience" />
-                            <MenuItem text=".projects()" icon="fas fa-tools" linkTo="projects" />
-                            <MenuItem text=".skills()" icon="fas fa-chart-bar" linkTo="skills" />
-                            <MenuItem text=".education()" icon="fas fa-graduation-cap" linkTo="education" />
-                            <MenuItem text=".contact()" icon="fas fa-comment-dots" linkTo="contact" />
-                            <MenuItem text=".resume()" icon="fas fa-file" linkTo="resume" />
-                        </div>
-                    </div>
-                </div>
-            </>
-        )
-    }
+        {/* Normal screen menu */}
+        <div
+          className="font-mono w-5/6 md:w-1/3 lg:w-1/4 2xl:w-1/5 shadow-2xl fixed h-screen z-40 bg-gray-200 dark:bg-gray-800 transition-transform md:transition-none md:transition-colors"
+          style={
+            this.state.menuActive ? {} : { transform: "translateX(-105%)" }
+          }
+        >
+          <div className="hidden md:block border-b-4 border-secondary dark:border-secondaryDark text-center">
+            <div className="text-primary dark:text-blue-300 font-bold text-2xl inline-block py-5 font-sans">
+              danielmelchor.com
+            </div>
+          </div>
+          <div className="md:top-1/2 md:absolute transform md:-translate-y-2/4 w-full">
+            <div className="w-full pt-5 flex flex-wrap items-center justify-center hidden md:block">
+              <a
+                className="pl-5 text-xl w-full text-gray-800 dark:text-gray-300"
+                href="/"
+              >
+                <span className="text-green-900 dark:text-blue-300">class</span>{" "}
+                User(
+                <span className="text-secondary dark:text-secondaryDark">
+                  D<span className="hidden xl:inline">aniel</span>M
+                </span>
+                ):
+              </a>
+            </div>
+            <div className="pt-10 md:pt-2 xl:pt-5">
+              <div className="md:hidden z-20 w-full mb-2 block px-3 text-center">
+                <p className="text-3xl inline-block align-middle text-primary dark:text-gray-300">
+                  Menu
+                </p>
+                <i
+                  onClick={() => this.toggleMenu()}
+                  className="fas fa-times text-3xl float-right inline-block align-middle text-primary dark:text-primaryDark"
+                ></i>
+              </div>
+              <MenuItem
+                text=".welcome()"
+                toggleMenuFunc={this.toggleMenu}
+                icon="fab fa-creative-commons-zero"
+                linkTo="welcome"
+              />
+              <MenuItem text=".about()" icon="fas fa-user" linkTo="about" />
+              <MenuItem
+                text=".experience()"
+                toggleMenuFunc={this.toggleMenu}
+                icon="fas fa-user-tie"
+                linkTo="experience"
+              />
+              <MenuItem
+                text=".projects()"
+                toggleMenuFunc={this.toggleMenu}
+                icon="fas fa-tools"
+                linkTo="projects"
+              />
+              <MenuItem
+                text=".skills()"
+                toggleMenuFunc={this.toggleMenu}
+                icon="fas fa-chart-bar"
+                linkTo="skills"
+              />
+              <MenuItem
+                text=".education()"
+                toggleMenuFunc={this.toggleMenu}
+                icon="fas fa-graduation-cap"
+                linkTo="education"
+              />
+              <MenuItem
+                text=".contact()"
+                toggleMenuFunc={this.toggleMenu}
+                icon="fas fa-comment-dots"
+                linkTo="contact"
+              />
+              <MenuItem
+                text=".resume()"
+                toggleMenuFunc={this.toggleMenu}
+                icon="fas fa-file"
+                linkTo="resume"
+              />
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
