e53c7c6e3aa45a9177fdb794542fb2d2c8a00d1e
Daniel Melchor <danielmelchor@localhost.localdomain>
2021-07-21 17:37:57 +0200

    Updates

---
+++ static/js/5.b31eff11.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpwebsite=this.webpackJsonpwebsite||[]).push([[5],{723:function(t,e,i){"use strict";i.r(e),e.default=i.p+"static/media/1-important-extraction.03f507d6.md"}}]);
+//# sourceMappingURL=5.b31eff11.chunk.js.map
--- static/js/5.cfbb7b1f.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpwebsite=this.webpackJsonpwebsite||[]).push([[5],{720:function(t,e,i){"use strict";i.r(e),e.default=i.p+"static/media/1-important-extraction.03f507d6.md"}}]);
-//# sourceMappingURL=5.cfbb7b1f.chunk.js.map
