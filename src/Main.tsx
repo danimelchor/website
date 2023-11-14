8a085c690d63d157b2e39c3f19147163f76c8863
danimelchor <dmelchor@pm.me>
2023-11-14 02:03:08 -0500

    fix minimize

--- src/Main.tsx
+++ src/Main.tsx
@@ -22,6 +22,7 @@ function Main() {
     if (newApp && newApp !== app) {
       setApp(newApp);
     } else {
+      setApp(null);
     }
 
     // Update URL
--- src/apps/About.tsx
+++ src/apps/About.tsx
@@ -3,7 +3,7 @@ const About = () => {
     <div className="flex flex-col items-center justify-center h-full w-full">
       <h1 className="text-3xl md:text-4xl lg:text-5xl font-extrabold leading-[1.14em] w-11/12 md:w-5/6 lg:w-1/2 text-center dark:text-slate-50">
         Hi! 👋 My name is{" "}
-        <span className="from-blue-600 dark:from-red-500 to-emerald-500 dark:to-orange-400 bg-gradient-to-r bg-clip-text text-transparent">
+        <span className="transition-all from-blue-600 dark:from-emerald-500 to-emerald-500 dark:to-blue-600 bg-gradient-to-r bg-clip-text text-transparent">
           Daniel
         </span>{" "}
         and I am a Software Engineer.
