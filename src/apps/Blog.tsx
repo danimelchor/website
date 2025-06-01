eeb3726f07037b5d44527545c70bbdc811fc104e
danimelchor <dmelchor@pm.me>
2025-06-01 15:10:48 -0400

    fix padding

--- src/apps/Blog.tsx
+++ src/apps/Blog.tsx
@@ -118,7 +118,7 @@ function BlogPostList({
         </div>
       </div>
 
-      <div className="grid grid-cols-2 gap-4">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
         {articles.map(([name, article]) => (
           <BlogPostItem
             article={article}
