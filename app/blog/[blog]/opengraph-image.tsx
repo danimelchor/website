8dfed579726d8dd3f7f52814c426755afb570307
danimelchor <dmelchor@pm.me>
2025-06-02 18:28:24 -0400

    opengraph

---
+++ app/blog/[blog]/opengraph-image.tsx
@@ -0,0 +1,39 @@
+import { getPost } from "@/lib/api";
+import { ImageResponse } from "next/og";
+
+export const alt = "Daniel Melchor";
+export const size = {
+  width: 1200,
+  height: 630,
+};
+export const contentType = "image/png";
+
+export default async function Image({
+  params,
+}: {
+  params: Promise<{ blog: string }>;
+}) {
+  const { blog } = await params;
+  const article = await getPost(blog);
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 48,
+          background: "white",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {article.title}
+      </div>
+    ),
+    {
+      ...size,
+    },
+  );
+}
