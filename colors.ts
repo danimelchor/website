ed60cd1c0ce4cc88d7854125f8b4e128fa2f1ddd
danimelchor <dmelchor@pm.me>
2025-06-03 23:14:54 -0400

    lighter colors

--- colors.ts
+++ colors.ts
@@ -1,8 +1,4 @@
-type Mapping = {
-  [key: string]: string;
-};
-
-export const COLOR_TO_SHADOW: Mapping = {
+export const COLOR_TO_SHADOW: Record<string, string> = {
   indigo: "shadow-indigo-300 dark:shadow-indigo-700",
   orange: "shadow-orange-300 dark:shadow-orange-700",
   cyan: "shadow-cyan-300 dark:shadow-cyan-700",
@@ -14,36 +10,36 @@ export const COLOR_TO_SHADOW: Mapping = {
   violet: "shadow-violet-300 dark:shadow-violet-700",
 };
 
-export const COLOR_TO_BG: Mapping = {
+export const COLOR_TO_BG: Record<string, string> = {
   indigo:
-    "to-indigo-100 hover:to-indigo-200 dark:to-indigo-900 dark:hover:to-indigo-800",
+    "to-indigo-200 hover:to-indigo-300 dark:to-indigo-900 dark:hover:to-indigo-800",
   orange:
-    "to-orange-100 hover:to-orange-200 dark:to-orange-900 dark:hover:to-orange-800",
-  cyan: "to-cyan-100 hover:to-cyan-200 dark:to-cyan-900 dark:hover:to-cyan-800",
+    "to-orange-200 hover:to-orange-300 dark:to-orange-900 dark:hover:to-orange-800",
+  cyan: "to-cyan-200 hover:to-cyan-300 dark:to-cyan-900 dark:hover:to-cyan-800",
   emerald:
-    "to-green-100 hover:to-green-200 dark:to-green-900 dark:hover:to-green-800",
-  rose: "to-rose-100 hover:to-rose-200 dark:to-rose-900 dark:hover:to-rose-800",
+    "to-green-200 hover:to-green-300 dark:to-green-900 dark:hover:to-green-800",
+  rose: "to-rose-200 hover:to-rose-300 dark:to-rose-900 dark:hover:to-rose-800",
   slate:
-    "to-slate-200 dark:to-slate-800 hover:to-slate-300 dark:hover:to-slate-700",
-  gray: "to-gray-200 dark:to-gray-800 hover:to-gray-300 dark:hover:to-gray-700",
-  blue: "to-blue-100 dark:to-blue-900 hover:to-blue-200 dark:hover:to-blue-800",
+    "to-slate-300 dark:to-slate-800 hover:to-slate-400 dark:hover:to-slate-700",
+  gray: "to-gray-300 dark:to-gray-800 hover:to-gray-400 dark:hover:to-gray-700",
+  blue: "to-blue-200 dark:to-blue-900 hover:to-blue-300 dark:hover:to-blue-800",
   violet:
-    "to-violet-100 dark:to-violet-900 hover:to-violet-200 dark:hover:to-violet-800",
+    "to-violet-200 dark:to-violet-900 hover:to-violet-300 dark:hover:to-violet-800",
 };
 
-export const COLOR_TO_IMG_BG: Mapping = {
-  indigo: "bg-indigo-300 dark:bg-indigo-700",
-  orange: "bg-orange-300 dark:bg-orange-700",
-  cyan: "bg-cyan-300 dark:bg-cyan-700",
-  emerald: "bg-emerald-300 dark:bg-emerald-700",
-  rose: "bg-rose-300 dark:bg-rose-700",
-  slate: "bg-slate-300 dark:bg-slate-700",
-  gray: "bg-gray-300 dark:bg-gray-700",
-  blue: "bg-blue-300 dark:bg-blue-700",
-  violet: "bg-violet-300 dark:bg-violet-700",
+export const COLOR_TO_IMG_BG: Record<string, string> = {
+  indigo: "bg-indigo-400 dark:bg-indigo-600",
+  orange: "bg-orange-400 dark:bg-orange-600",
+  cyan: "bg-cyan-400 dark:bg-cyan-600",
+  emerald: "bg-emerald-400 dark:bg-emerald-600",
+  rose: "bg-rose-400 dark:bg-rose-600",
+  slate: "bg-slate-400 dark:bg-slate-600",
+  gray: "bg-gray-400 dark:bg-gray-600",
+  blue: "bg-blue-400 dark:bg-blue-600",
+  violet: "bg-violet-400 dark:bg-violet-600",
 };
 
-export const COLOR_TO_TEXT_COLOR: Mapping = {
+export const COLOR_TO_TEXT_COLOR: Record<string, string> = {
   indigo: "text-indigo-700 dark:text-indigo-300",
   orange: "text-orange-700 dark:text-orange-300",
   cyan: "text-cyan-700 dark:text-cyan-300",
@@ -55,7 +51,7 @@ export const COLOR_TO_TEXT_COLOR: Mapping = {
   violet: "text-violet-700 dark:text-violet-300",
 };
 
-export const COLOR_TO_RING_COLOR: Mapping = {
+export const COLOR_TO_RING_COLOR: Record<string, string> = {
   indigo: "ring-indigo-600/20 dark:ring-indigo-500/20",
   orange: "ring-orange-600/20 dark:ring-orange-500/20",
   cyan: "ring-cyan-600/20 dark:ring-cyan-500/20",
@@ -67,7 +63,7 @@ export const COLOR_TO_RING_COLOR: Mapping = {
   violet: "ring-violet-600/20 dark:ring-violet-500/20",
 };
 
-export const COLOR_TO_LIGHT_BG: Mapping = {
+export const COLOR_TO_LIGHT_BG: Record<string, string> = {
   indigo: "bg-indigo-50 dark:bg-indigo-950",
   orange: "bg-orange-50 dark:bg-orange-950",
   cyan: "bg-cyan-50 dark:bg-cyan-950",
